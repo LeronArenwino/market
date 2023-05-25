1214071123691923e69f2bc95d5250c0d91c9d71
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-05-24 21:17:01 -0500

    :tada: Initial commit!

---
+++ src/test/java/com/leronarenwino/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leronarenwino.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
