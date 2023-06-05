04be0f75f166cb34ba8ac61c50d2421d12bdf2d4
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-06-04 21:30:07 -0500

    :fire: [DELETE] Remove useless code

--- src/main/java/com/leronarenwino/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/leronarenwino/market/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,6 @@
 package com.leronarenwino.market.persistence.crud;
 
 import com.leronarenwino.market.persistence.entity.Producto;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
